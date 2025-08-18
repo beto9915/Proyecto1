f44e68db0f5e15d946c8c8008e46ef05d6d82481
beto9915 <alcs1491@gmail.com>
2025-08-17 18:34:27 -0600

    Version2

--- main.py
+++ main.py
@@ -17,15 +17,30 @@ class Inventario:
             print("codigo ya registrado...")
             print("\npresione ENTER para continuar..")
             input()
+            return
         else:
             nombre=input("Ingrese el nombre del producto: ")
             if nombre in self.inventario.values().nombre:
                 print("nombre ya registrado...")
                 print("\npresione ENTER para continuar...")
                 input()
+                return
             else:
                 categoria=input("Ingrese categoria: ")
                 precio=float(input("Ingrese el precio: "))
                 stock=int(input("Ingrese el stock disponible"))
         self.inventario[codigo]=Producto(codigo, nombre, categoria, precio, stock)
         print("Producto registrado con exito! "*3)
+    def actualizar_producto(self):
+        codigo=input("Ingrese el codigo de producto a buscar: ")
+        if codigo in self.inventario.keys():
+            stock=int(input("Ingrese nuevo stock: "))
+            precio=float(input("Ingrese nuevo precio: "))
+            self.inventario[codigo][stock]=stock
+            self.inventario[codigo][precio]=precio
+            print("Producto actualizado con exito! "*3)
+        else:
+            print("producto no encontrado...")
+            return
+    def eliminar_producto(self):
+        pass
